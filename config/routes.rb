7061e86e847fe5040df3922ba22d4aea6f13bb8c
Joris Kuehnast <joris@kuehnast.one>
2020-04-17 20:02:38 +0200

    background homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
-  get 'about', to: 'pages#about'
-  get 'contact', to: 'pages#contact'
+  get 'about', to: 'pages#about', as: :about
+  get 'contact', to: 'pages#contact', as: :contact
 end
